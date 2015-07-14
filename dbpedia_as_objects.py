30dd312ad8ffb62ad1a215ba6d927828704fbd2d
Nathann Cohen <nathann.cohen@gmail.com>
2015-07-14 15:38:00 +0200

    More cleaning+doc

--- dbpedia_as_objects.py
+++ dbpedia_as_objects.py
@@ -1,7 +1,39 @@
+r"""
+A dbpedia entry is an object, its properties are methods. The data is queried
+as you explore the objects.
+
+Some objects have a .sage() method to obtain the actual Sage object.
+
+EXAMPLE::
+
+    sage: g = DbpediaEntry("http://dbpedia.org/resource/Petersen_graph")
+    sage: g.girth()
+    5
+    sage: g.properties()
+    [DBpedia entry: Cubic graph,
+     DBpedia entry: Snark (graph theory),
+     DBpedia entry: Strongly regular graph,
+     DBpedia entry: Distance-transitive graph]
+
+    sage: g.subject()
+    [DBpedia entry: Category of Individual graphs,
+     DBpedia entry: Category of Regular graphs]
+
+    sage: individual = _[0]
+    sage: individual.elements()
+    [DBpedia entry: BiggsSmith graph,
+     DBpedia entry: TutteCoxeter graph,
+     DBpedia entry: Bull graph,
+     DBpedia entry: Double-star snark,
+     DBpedia entry: Truncated tetrahedron,
+     DBpedia entry: TutteCoxeter graph,
+     DBpedia entry: Tutte 12-cage,
+     ...
+"""
 from SPARQLWrapper import SPARQLWrapper, JSON, XML, N3, RDF, TURTLE
 from collections import defaultdict
 import rdflib
-from rdflib import URIRef
+from rdflib import URIRef, Namespace
 
 import logging
 logging.getLogger().setLevel(logging.ERROR)
@@ -9,7 +41,13 @@ logging.getLogger().setLevel(logging.ERROR)
 if 'local_data' not in locals():
     local_data = rdflib.Graph()
 
-rdf_type = URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
+rdfs    = Namespace("http://www.w3.org/2000/01/rdf-schema#")
+sagens  = Namespace("http://www.sagemath.org/#")
+dbpedia = Namespace("http://dbpedia.org/resource/")
+rdf     = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+dbpprop = Namespace("http://dbpedia.org/property/")
+dcterms = Namespace("http://purl.org/dc/terms/")
+skos    = Namespace("http://www.w3.org/2004/02/skos/core#")
 
 # If any (?s ?p ?o) gets added to local_data, then ALL (?s * *) must be added!
 # All or none.
@@ -49,11 +87,11 @@ def get_category_elements(URI):
     """
     add_to_local_db(r"""
     construct {
-       ?s <http://purl.org/dc/terms/subject> <URI>.
+       ?s dcterms:subject <URI>.
        ?s ?p ?o.
        }
     where {
-       ?s <http://purl.org/dc/terms/subject> <URI>.
+       ?s dcterms:subject <URI>.
        ?s ?p ?o.
      }
     """.replace("URI",URI))
@@ -73,11 +111,24 @@ def get_category_subcategories(URI):
      }
     """.replace("URI",URI))
 
+def value_to_sage(v):
+    r"""
+    Convert a db value into something Sage understands.
+    """
+    if isinstance(v,rdflib.URIRef):
+        return DbpediaEntry(v)
+    if v.isnumeric():
+        return eval(str(v))
+    else:
+        return v.toPython()
+
 class DbpediaEntry:
     r"""
     Represents an entry in DBpedia
     """
     def __init__(self,URI):
+        if isinstance(URI,rdflib.URIRef):
+            URI = URI.toPython()
         get_entry_from_dbpedia(URI)
         self._uriref = rdflib.URIRef(URI)
         d = defaultdict(list)
@@ -87,7 +138,7 @@ class DbpediaEntry:
             setattr(self,p,self._return_function(p))
 
         self._d = d
-        self._is_category = ((self._uriref, rdf_type, URIRef("http://www.w3.org/2004/02/skos/core#Concept"))
+        self._is_category = ((self._uriref, rdf.type, skos.Concept)
                              in local_data)
 
     def _return_function(self,param):
@@ -95,9 +146,7 @@ class DbpediaEntry:
         Helper function returning a dbpedia value
         """
         def the_function():
-            data = self._d[param]
-            data = [DbpediaEntry(x.toPython()) if isinstance(x,rdflib.URIRef) else x.toPython()
-                    for x in data]
+            data = [value_to_sage(x) for x in self._d[param]]
             return data[0] if (isinstance(data,list) and len(data)==1) else data
 
         return the_function
@@ -106,12 +155,14 @@ class DbpediaEntry:
         r"""
         Return a 'nice' representation of the object
         """
-        label = self.label()
+        try:
+            label = self.label()
+        except:
+            label = self._uriref.toPython()
+
         if isinstance(label,list):
-            if len(label) == 0:
-                label = self._uriref.toPython()
-            else:
-                label = label[0]
+            label = label[0]
+
         label = label.encode('ascii','ignore')
         return ("DBpedia entry: "+
                 ("Category of " if self._is_category else "")+
@@ -119,23 +170,27 @@ class DbpediaEntry:
 
     def elements(self):
         r"""
-        Return all elements in the category represented by self
+        Return all elements in the category represented by self.
+
+        Pre-loads all the elements in the db.
         """
         if not self._is_category:
             raise TypeError("This is not a Wikipedia category.")
         get_category_elements(self._uriref.toPython())
         return [DbpediaEntry(s.toPython()) for s,_,_ in
-                local_data.triples((None,rdflib.URIRef("http://purl.org/dc/terms/subject"),self._uriref))]
+                local_data.triples((None,dcterms.subject,self._uriref))]
 
     def subcategories(self):
         r"""
         Return all the subcategories of self
+
+        Pre-loads all subcategories in the db.
         """
         if not self._is_category:
             raise TypeError("This is not a Wikipedia category.")
         get_category_subcategories(self._uriref.toPython())
         return [DbpediaEntry(s.toPython()) for s,_,_ in
-                local_data.triples((None,rdflib.URIRef("http://www.w3.org/2004/02/skos/core#broader"),self._uriref))]
+                local_data.triples((None,skos.broader,self._uriref))]
 
     def sage(self):
         r"""
@@ -143,7 +198,7 @@ class DbpediaEntry:
         """
         translation_db=rdflib.Graph()
         translation_db.parse("graphs.ttl",format="n3")
-        for _,_,o in translation_db.triples((self._uriref,rdflib.URIRef("http://www.sagemath.org/#build"),None)):
+        for _,_,o in translation_db.triples((self._uriref,sagens.build,None)):
             return eval(o)
         raise RuntimeError("No translation found")
 
