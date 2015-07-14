cb166df4173d2cc27dfbc7c944af940e6be29681
Nathann Cohen <nathann.cohen@gmail.com>
2015-07-14 13:30:47 +0200

    some doc and UI

--- dbpedia_as_objects.py
+++ dbpedia_as_objects.py
@@ -1,10 +1,13 @@
+from SPARQLWrapper import SPARQLWrapper, JSON, XML, N3, RDF, TURTLE
 from collections import defaultdict
 import rdflib
 from rdflib import URIRef
 
+import logging
+logging.getLogger().setLevel(logging.ERROR)
+
 if 'local_data' not in locals():
     local_data = rdflib.Graph()
-
 
 rdf_type = URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
 
@@ -13,17 +16,22 @@ rdf_type = URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
 
 @cached_function
 def add_to_local_db(request):
-    from SPARQLWrapper import SPARQLWrapper, JSON, XML, N3, RDF, TURTLE
-    import rdflib
+    r"""
+    Add to the local db the result of a given SPARQL query
+    """
+    global local_data
     sparql = SPARQLWrapper("http://live.dbpedia.org/sparql")
     sparql.setQuery(request)
     sparql.setReturnFormat(RDF)
     q = sparql.query()
-    q = results = q.convert()
-    global local_data
+    sage.q = q
+    q = q.convert()
     local_data = local_data+q
 
 def get_entry_from_dbpedia(URI):
+    r"""
+    Fetch all triples (<URI>,p,o)
+    """
     if rdflib.URIRef(URI) in local_data.subjects():
         return
     add_to_local_db(r"""
@@ -36,6 +44,9 @@ def get_entry_from_dbpedia(URI):
     """.replace("URI",URI))
 
 def get_category_elements(URI):
+    r"""
+    Fetch all triples (?s ?p ?o) where ?s has category <URI>
+    """
     add_to_local_db(r"""
     construct {
        ?s <http://purl.org/dc/terms/subject> <URI>.
@@ -48,6 +59,9 @@ def get_category_elements(URI):
     """.replace("URI",URI))
 
 def get_category_subcategories(URI):
+    r"""
+    Fetch all triples (?s ?p ?o) where ?s has supercategory <URI>
+    """
     add_to_local_db(r"""
     construct {
        ?s <http://www.w3.org/2004/02/skos/core#broader> <URI>.
@@ -60,22 +74,28 @@ def get_category_subcategories(URI):
     """.replace("URI",URI))
 
 class DbpediaEntry:
+    r"""
+    Represents an entry in DBpedia
+    """
     def __init__(self,URI):
         get_entry_from_dbpedia(URI)
-        self.uriref = rdflib.URIRef(URI)
+        self._uriref = rdflib.URIRef(URI)
         d = defaultdict(list)
-        for s,p,o in local_data.triples((self.uriref,None,None)):
+        for s,p,o in local_data.triples((self._uriref,None,None)):
             p = p.encode('ascii','ignore').split('/')[-1].split('#')[-1]
             d[p].append(o)
-        self.d = d
-        self._is_category = ((self.uriref, rdf_type, URIRef("http://www.w3.org/2004/02/skos/core#Concept"))
-                             in local_data)
-        for p in d:
             setattr(self,p,self._return_function(p))
 
+        self._d = d
+        self._is_category = ((self._uriref, rdf_type, URIRef("http://www.w3.org/2004/02/skos/core#Concept"))
+                             in local_data)
+
     def _return_function(self,param):
+        r"""
+        Helper function returning a dbpedia value
+        """
         def the_function():
-            data = self.d[param]
+            data = self._d[param]
             data = [DbpediaEntry(x.toPython()) if isinstance(x,rdflib.URIRef) else x.toPython()
                     for x in data]
             return data[0] if (isinstance(data,list) and len(data)==1) else data
@@ -83,10 +103,13 @@ class DbpediaEntry:
         return the_function
 
     def __repr__(self):
+        r"""
+        Return a 'nice' representation of the object
+        """
         label = self.label()
         if isinstance(label,list):
             if len(label) == 0:
-                label = self.uriref.toPython()
+                label = self._uriref.toPython()
             else:
                 label = label[0]
         label = label.encode('ascii','ignore')
@@ -95,23 +118,32 @@ class DbpediaEntry:
                 label)
 
     def elements(self):
+        r"""
+        Return all elements in the category represented by self
+        """
         if not self._is_category:
             raise TypeError("This is not a Wikipedia category.")
-        get_category_elements(self.uriref.toPython())
+        get_category_elements(self._uriref.toPython())
         return [DbpediaEntry(s.toPython()) for s,_,_ in
-                local_data.triples((None,rdflib.URIRef("http://purl.org/dc/terms/subject"),self.uriref))]
+                local_data.triples((None,rdflib.URIRef("http://purl.org/dc/terms/subject"),self._uriref))]
 
     def subcategories(self):
+        r"""
+        Return all the subcategories of self
+        """
         if not self._is_category:
             raise TypeError("This is not a Wikipedia category.")
-        get_category_elements(self.uriref.toPython())
+        get_category_subcategories(self._uriref.toPython())
         return [DbpediaEntry(s.toPython()) for s,_,_ in
-                local_data.triples((None,rdflib.URIRef("http://www.w3.org/2004/02/skos/core#broader"),self.uriref))]
+                local_data.triples((None,rdflib.URIRef("http://www.w3.org/2004/02/skos/core#broader"),self._uriref))]
 
     def sage(self):
+        r"""
+        Return the Sage function associated with self (if known/available).
+        """
         translation_db=rdflib.Graph()
         translation_db.parse("graphs.ttl",format="n3")
-        for _,_,o in translation_db.triples((self.uriref,rdflib.URIRef("http://www.sagemath.org/#build"),None)):
+        for _,_,o in translation_db.triples((self._uriref,rdflib.URIRef("http://www.sagemath.org/#build"),None)):
             return eval(o)
         raise RuntimeError("No translation found")
 
